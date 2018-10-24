3aed37cf125ccae6217363cd0c4f64e83ff55fff
Jari Otranen <jari.otranen@zalando.fi>
2018-10-24 15:53:40 +0300

    ch01 ex04

--- ch01/ex04.go
+++ ch01/ex04.go
@@ -1,3 +1,19 @@
 package ch01
 
 // Find the smallest element in the list
+func Min(in []int) int {
+
+	if len(in) == 0 {
+		return 0
+	}
+
+	min := in[0]
+
+	for _, value := range in {
+		if value < min {
+			min = value
+		}
+	}
+
+	return min
+}
--- ch01/ex04_test.go
+++ ch01/ex04_test.go
@@ -1,1 +1,21 @@
 package ch01
+
+import "testing"
+
+func TestEx04(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want int
+	}{
+		{in: []int{}, want: 0},
+		{in: []int{1, 2, 3, 4, 5}, want: 1},
+		{in: []int{99}, want: 99},
+		{in: []int{7, 2, 11, 2, 5}, want: 2},
+		{in: []int{7, 7, 0, 1, 5}, want: 0},
+	} {
+		got := Min(c.in)
+		if got != c.want {
+			t.Errorf("Average(%v) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
