2411f87640f1706bc0da2b2fc8f6fb03967fd520
Jari Otranen <jari.otranen@zalando.fi>
2018-10-24 23:15:19 +0300

    ch01 ex06

--- ch01/ex06.go
+++ ch01/ex06.go
@@ -1,32 +1,39 @@
 package ch01
 
+import "fmt"
+
 // Using AllPermutation function discussed before, write a function, which give only distinct solutions
-//func Permutation(data []int, i int, length int) {
-//	if length == 1 {
-//		PrintSlice(data)
-//		return
-//	}
-//	for j := i; j < length; j++ {
-//		swap(data, i, j)
-//		Permutation(data, i+1, length)
-//		swap(data, i, j)
-//	}
-//}
-//
-//func swap(data []int, x int, y int) {
-//	data[x], data[y] = data[y], data[x]
-//}
-//
-//func main() {
-//	var data [5]int
-//
-//	for i := 0; i < 5; i++ {
-//		data[i] = i
-//	}
-//
-//	Permutation(data[:], 0, 5)
-//}
-//
-//func PrintSlice(data []int) {
-//	fmt.Printf("%v :: len=%d cap-=%d \n", data, len(data), cap(data))
-//}
+func DistinctPermutations(in []int) []string {
+	var m map[string]struct{}
+	m = make(map[string]struct{})
+
+	Permutation(in, 0, len(in), m)
+
+	var res []string
+	for k := range m {
+		res = append(res, k)
+	}
+
+	return res
+}
+
+func Permutation(data []int, i int, length int, m map[string]struct{}) {
+	//PrintSlice(data)
+	if length == i {
+		m[fmt.Sprintf("%v", data)] = struct{}{}
+		return
+	}
+	for j := i; j < length; j++ {
+		swap(data, i, j)
+		Permutation(data, i+1, length, m)
+		swap(data, i, j)
+	}
+}
+
+func swap(data []int, x int, y int) {
+	data[x], data[y] = data[y], data[x]
+}
+
+func PrintSlice(data []int) {
+	fmt.Printf("%v :: len=%d cap=%d \n", data, len(data), cap(data))
+}
--- ch01/ex06_test.go
+++ ch01/ex06_test.go
@@ -1,1 +1,21 @@
 package ch01
+
+import (
+	"testing"
+)
+
+func TestEx06(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want int
+	}{
+		{in: []int{1, 1}, want: 1},
+		{in: []int{1, 2, 3}, want: 6},
+		{in: []int{1, 1, 3}, want: 3},
+	} {
+		got := DistinctPermutations(c.in)
+		if len(got) != c.want {
+			t.Errorf("DistinctPermutations(%v) == %v, want %v", c.in, len(got), c.want)
+		}
+	}
+}
