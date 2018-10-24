d92ce59b9071834a676fa4b6c9c549bff3becf7a
Jari Otranen <jari.otranen@zalando.fi>
2018-10-24 21:53:35 +0300

    ch01 ex05

--- ch01/ex05.go
+++ ch01/ex05.go
@@ -1,3 +1,33 @@
 package ch01
 
 // Find the second largest number in the list
+func SecondLargest(in []int) int {
+
+	// -1 if can not find second largest
+	if len(in) < 2 || (len(in) == 2 && in[0] == in[1]) {
+		return -1
+	}
+
+	max, max2 := func(a int, b int) (max int, min int) {
+		if a > b {
+			return a, b
+		} else {
+			return b, a
+		}
+	}(in[0], in[1])
+
+	for _, value := range in {
+		if value > max {
+			max2 = max
+			max = value
+		} else if value > max2 && value < max {
+			max2 = value
+		}
+	}
+
+	if max2 == max {
+		return -1
+	}
+
+	return max2
+}
--- ch01/ex05_test.go
+++ ch01/ex05_test.go
@@ -1,1 +1,21 @@
 package ch01
+
+import "testing"
+
+func TestEx05(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want int
+	}{
+		{in: []int{}, want: -1},
+		{in: []int{1, 2, 3, 4, 5}, want: 4},
+		{in: []int{99}, want: -1},
+		{in: []int{7, 2, 11, 4, 5}, want: 7},
+		{in: []int{7, 7, 11, 11, 5}, want: 7},
+	} {
+		got := SecondLargest(c.in)
+		if got != c.want {
+			t.Errorf("Average(%v) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
