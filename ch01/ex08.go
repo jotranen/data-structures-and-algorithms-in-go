18e1d8ded7caa6fd088c9c24f4ea5e7a991601d3
Jari Otranen <jari.otranen@zalando.fi>
2018-10-25 00:03:16 +0300

    ch01 ex08

--- ch01/ex08.go
+++ ch01/ex08.go
@@ -1,4 +1,38 @@
 package ch01
 
-// Print all the maxima's in a list (A value is maximum if the value before and after its index are smaller than it is or does not exist)
-// Hint: start traversing list form the end and keep track of the max element. if we encounter an element whose value is grater then max, print the lement and update max
+// Print all the maximum's in a list
+// (A value is maximum if the value before and after its index are smaller than it is or does not exist)
+// Hint: start traversing list form the end and keep track of the max element.
+// If we encounter an element whose value is greater then max, print the element and update max
+func Maximums(in []int) []int {
+
+	if len(in) == 0 {
+		return []int{}
+	}
+	if len(in) == 1 {
+		return []int{in[0]}
+	}
+
+	var maximums []int
+	var max int
+	var prev bool
+
+	max = in[len(in)-1]
+	prev = false
+
+	for i := len(in) - 1; i >= 0; i-- {
+		if i == 0 && !prev && in[0] > in[1] {
+			maximums = append(maximums, in[0])
+		} else if in[i] < max && !prev {
+			maximums = append(maximums, max)
+			prev = true
+		} else if in[i] > max {
+			max = in[i]
+			prev = false
+		} else {
+			prev = false
+		}
+	}
+
+	return maximums
+}
--- ch01/ex08_test.go
+++ ch01/ex08_test.go
@@ -1,1 +1,29 @@
 package ch01
+
+import (
+	"testing"
+)
+
+func TestEx08(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want []int
+	}{
+		{in: []int{1, 2, 1}, want: []int{2}},
+		{in: []int{2, 2, 1}, want: []int{}},
+		{in: []int{3, 2, 1}, want: []int{3}},
+		{in: []int{3, 2, 1, 8}, want: []int{8, 3}},
+		{in: []int{3, 2, 1, 8, 7}, want: []int{8, 3}},
+		{in: []int{}, want: []int{}},
+		{in: []int{99}, want: []int{99}},
+		{in: []int{1, 1, 1, 1, 1, 1, 1, 1}, want: []int{}},
+	} {
+		got := Maximums(c.in)
+
+		for i := range got {
+			if c.want[i] != got[i] {
+				t.Errorf("Maximums(%v) == %v, want %v", c.in, got[i], c.want[i])
+			}
+		}
+	}
+}
