4ebc73e6be462cadde09677d59771c844494bd4b
Jari Otranen <jari.otranen@zalando.fi>
2018-10-26 00:15:21 +0300

    ch01 ex11

--- ch01/ex11.go
+++ ch01/ex11.go
@@ -1,6 +1,26 @@
 package ch01
 
-// Given a list of 0s and 1s. We need to sort it so that all the 0s are before adll the 1s.
-//	Hint: Use two variable, start and end. Start set to 0 and end set to (n1). Increment start and decrement end.
+import "fmt"
+
+// Given a list of 0s and 1s. We need to sort it so that all the 0s are before all 1s.
+//	Hint: Use two variables, start and end.
+//  Start set to 0 and end set to (n-1). Increment start and decrement end.
 //	Swap the values stored at arr[start] and arr[end] only when arr[start] == 1 and arr[end] == 0.
-//Stop when start is equal to end or styart is greater than end.
+//  Stop when start is equal to end or start is greater than end.
+
+func Sort_0_1(in []int) []int {
+
+	cpy := make([]int, len(in))
+	copy(cpy, in)
+
+	for start, end := 0, len(cpy)-1; start <= end; end-- {
+		if cpy[start] == 1 && cpy[end] == 0 {
+			cpy[start], cpy[end] = cpy[end], cpy[start]
+			fmt.Println(cpy)
+		}
+		if start < end && cpy[start+1] != 0 {
+			start++
+		}
+	}
+	return cpy
+}
--- ch01/ex11_test.go
+++ ch01/ex11_test.go
@@ -1,1 +1,33 @@
 package ch01
+
+import "testing"
+
+func TestEx11(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want []int
+	}{
+		//{in: []int{0, 1}, want: []int{0, 1}},
+		//{in: []int{1, 0}, want: []int{0, 1}},
+		{in: []int{1, 1, 0, 1, 1, 0}, want: []int{0, 0, 1, 1, 1, 1}},
+	} {
+
+		intComparer := func(a []int, b []int) bool {
+			if len(a) != len(b) {
+				return false
+			}
+			for i, v := range a {
+				if v != b[i] {
+					return false
+				}
+			}
+			return true
+		}
+
+		got := Sort_0_1(c.in)
+
+		if !intComparer(got, c.want) {
+			t.Errorf("Sort_0_1(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
