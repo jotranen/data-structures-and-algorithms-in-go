2be1ba63a0da931af13b295125fbc1a90ee33714
= <=>
2018-10-25 14:56:07 +0300

    ch01 ex10

--- ch01/ex10.go
+++ ch01/ex10.go
@@ -1,5 +1,18 @@
 package ch01
 
-// Reverse a list in-place (You cannot use any additional list or other vards Space Complexity should be O(1))
+// Reverse a list in-place (You cannot use any additional list or other variables Space Complexity should be O(1))
 // Hint: Use two variable, start and end. Start set to 0 and end set to (n-1). Increment start and decrement end. Swap the values stored at arr[start] and arr[end]. Stop when
 // start is equal to end or start is greater than end.
+func Reverse(in []int) []int {
+
+	start := 0
+	end := len(in) - 1
+
+	for start <= end {
+		in[start], in[end] = in[end], in[start]
+		start++
+		end--
+	}
+
+	return in
+}
--- ch01/ex10_test.go
+++ ch01/ex10_test.go
@@ -1,1 +1,26 @@
 package ch01
+
+import (
+	"testing"
+)
+
+func TestEx10(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want []int
+	}{
+		{in: []int{1}, want: []int{1}},
+		{in: []int{2, 2, 1}, want: []int{1, 2, 2}},
+		{in: []int{2, 2, 1}, want: []int{1, 2, 2}},
+		{in: []int{2, 2, 1, 3}, want: []int{3, 1, 2, 2}},
+	} {
+		got := Reverse(c.in)
+
+		for i := range got {
+			if c.want[i] != got[i] {
+				t.Errorf("Reverse(%v) == %v, want %v", c.in, got, c.want)
+				break
+			}
+		}
+	}
+}
