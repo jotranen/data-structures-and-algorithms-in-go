cb914f30dea7148151786bf970d3cfed8b319404
Jari Otranen <jari.otranen@zalando.fi>
2018-10-25 12:50:42 +0300

    ch01 ex09

--- ch01/ex09.go
+++ ch01/ex09.go
@@ -1,3 +1,56 @@
 package ch01
 
-// Give a list of intervals, merge all overlapping intervals Input{[1,4], [3,6],[8,10]} Output{[1,6], [8,10]}
+// Given a list of intervals, merge all overlapping intervals Input{[1,4], [3,6],[8,10]} Output{[1,6], [8,10]}
+func MergeIntervals(in [][]int) [][]int {
+
+	var max int
+
+	var res [][]int
+
+	// find end point for intervals
+	for _, array := range in {
+		if array[len(array)-1] > max {
+			max = array[len(array)-1]
+		}
+	}
+
+	// default for bool is false
+	intervals := make([]bool, max+1)
+
+	// create array and flag active entries in the who range
+	for _, array := range in {
+		start := array[0]
+		end := array[len(array)-1]
+		for start <= end {
+			intervals[start] = true
+			start++
+		}
+	}
+
+	var active bool
+	var start int
+	var end int
+
+	// split intervals based on active flag
+	for i := 1; i <= max; i++ {
+		if !active {
+			if intervals[i] {
+				active = true
+				start = i
+				end = i
+			}
+		} else {
+			if intervals[i] && i == len(intervals)-1 {
+				res = append(res, []int{start, end + 1})
+				active = false
+			} else if intervals[i] {
+				end = i
+			} else {
+				active = false
+				res = append(res, []int{start, end})
+			}
+		}
+	}
+
+	return res
+}
--- ch01/ex09_test.go
+++ ch01/ex09_test.go
@@ -1,1 +1,26 @@
 package ch01
+
+import (
+	"testing"
+)
+
+func TestEx09(t *testing.T) {
+	for _, c := range []struct {
+		in   [][]int
+		want [][]int
+	}{
+		{in: [][]int{{1, 4}, {3, 6}, {8, 10}}, want: [][]int{{1, 6}, {8, 10}}},
+		{in: [][]int{{1, 4}, {3, 6}}, want: [][]int{{1, 6}}},
+		{in: [][]int{{1, 4}}, want: [][]int{{1, 4}}},
+	} {
+		got := MergeIntervals(c.in)
+
+		for i, value := range got {
+			for j, inner := range value {
+				if inner != c.want[i][j] {
+					t.Errorf("MergeIntervals(%v) == %v, want %v", c.in, got, c.want)
+				}
+			}
+		}
+	}
+}
