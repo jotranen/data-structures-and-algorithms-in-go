551f314046625897c8cf566eaf80444d109cd8bb
jotranen <jari.otranen@gmail.com>
2018-10-26 15:58:48 +0300

    ch01 ex12

--- ch01/ex12.go
+++ ch01/ex12.go
@@ -2,3 +2,23 @@ package ch01
 
 // Given and array of 0s, 1s, and 2s. When need to stort it so that all the 0s are before all the 1s and all the 1s are before 2s.
 // Hint: same ass above first think of 0s and 1s as one groupd and move all the 2s on the right side. Then do a second pass over the array to sort 0s and 1s.
+func Sort_0_1_2(in []int) []int {
+	cpy := make([]int, len(in))
+	copy(cpy, in)
+
+	for start, end := 0, len(cpy)-1; start <= end; end-- {
+		if (cpy[start] == 1 || cpy[start] == 2) && cpy[end] == 0 {
+			cpy[start], cpy[end] = cpy[end], cpy[start]
+		}
+		if start < end && cpy[start+1] != 0 {
+			start++
+		}
+	}
+	for start, end := 0, len(cpy)-1; start <= end; start++ {
+		if cpy[start] == 2 && cpy[end] != 2 {
+			cpy[start], cpy[end] = cpy[end], cpy[start]
+			end--
+		}
+	}
+	return cpy
+}
--- ch01/ex12_test.go
+++ ch01/ex12_test.go
@@ -1,1 +1,33 @@
 package ch01
+
+import "testing"
+
+func TestEx12(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want []int
+	}{
+		{in: []int{0, 1, 2}, want: []int{0, 1, 2}},
+		{in: []int{1, 0, 2}, want: []int{0, 1, 2}},
+		{in: []int{1, 1, 2, 1, 1, 0}, want: []int{0, 1, 1, 1, 1, 2}},
+	} {
+
+		intComparer := func(a []int, b []int) bool {
+			if len(a) != len(b) {
+				return false
+			}
+			for i, v := range a {
+				if v != b[i] {
+					return false
+				}
+			}
+			return true
+		}
+
+		got := Sort_0_1_2(c.in)
+
+		if !intComparer(got, c.want) {
+			t.Errorf("Sort_0_1_2(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
