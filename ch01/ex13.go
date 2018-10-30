40ad67bc2771380b5fe4dfe942998455b14e7d0c
jotranen <jari.otranen@gmail.com>
2018-10-30 14:14:39 +0200

    ch01 ex13

--- ch01/ex13.go
+++ ch01/ex13.go
@@ -1,9 +1,32 @@
 package ch01
 
 // Find the duplicate elements in a list of size n where each element is inthe range 0 to n-1
-//Hint: Approach 1: Compare each element with all the elements of the list (using two loops O(n2) solution)
-//Approach 2: Maint a Hash-Table. Set the has value to 1 if we encounter the element for the first time. When we same value again we can see that the hash value is already 1 so we can print tht value
-//O(n) solutio, but additional space if required
-//Approach 3: We will explit the constraing "every element is tin the tnage 0 to n-1" We can tkae a list arr[] or size n and set all the elements to 0. Whenever we get a value say val1.
-//	We will inclement the value at arr[val1] index by 1. Int then, we can traverse the list arr and print repeated values.
-//	Additions space complexity will be O(n) whicl will be less than Hash-Table approach.
+
+//	Hint:
+// 		Approach 1: Compare each element with all the elements of the list (using two loops O(n2) solution)
+//		Approach 2: Maintain a Hash-Table. Set the has value to 1 if we encounter the element for the first time.
+// 					When we same value again we can see that the hash value is already 1 so we can print tht value.
+//					O(n) solution, but additional space if required.
+//		Approach 3: We will explicit the constraint "every element is in the range 0 to n-1"
+// 					We can take a list arr[] or size n and set all the elements to 0. Whenever we get a value say val1.
+//					We will inclement the value at arr[val1] index by 1. Int then, we can traverse the list arr and print repeated values.
+//					Additions space complexity will be O(n) which will be less than Hash-Table approach.
+
+func FindDuplicated(in []int) []int {
+
+	hm := make(map[int]int)
+
+	for _, value := range in {
+		hm[value] += 1
+	}
+
+	var res []int
+
+	for key, val := range hm {
+		if val >= 2 {
+			res = append(res, key)
+		}
+	}
+
+	return res
+}
--- ch01/ex13_test.go
+++ ch01/ex13_test.go
@@ -1,1 +1,39 @@
 package ch01
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestEx13(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want []int
+	}{
+		{in: []int{0, 1, 2}, want: []int{}},
+		{in: []int{1, 1, 2}, want: []int{1}},
+		{in: []int{6, 6, 1, 1, 2, 1, 1, 0, 2, 7, 7}, want: []int{1, 2, 6, 7}},
+	} {
+
+		intComparer := func(a []int, b []int) bool {
+			if len(a) != len(b) {
+				return false
+			}
+			for i, v := range a {
+				if v != b[i] {
+					return false
+				}
+			}
+			return true
+		}
+
+		got := FindDuplicated(c.in)
+
+		// make it a bit more testable
+		sort.Ints(got)
+
+		if !intComparer(got, c.want) {
+			t.Errorf("Sort_0_1_2(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
