767df456b5b65481867dceba2279d549b1eb8756
jotranen <jari.otranen@gmail.com>
2018-10-31 17:54:08 +0200

    ch01 ex16

--- ch01/ex16.go
+++ ch01/ex16.go
@@ -1,3 +1,12 @@
 package ch01
 
-// Write a method to find the sum of every number in an int number. Example input = 1984 output should be 32 (1+9+8+4)
+// Write a method to find the sum of every number in an int number. Example input = 1984 output should be 22 (1+9+8+4)
+func SumNumbers(in string) int {
+
+	var sum int
+	for _, val:= range in {
+		sum = sum + int(val) - 48 	// ascii 0 value is 48
+	}
+
+	return sum
+}
--- ch01/ex16_test.go
+++ ch01/ex16_test.go
@@ -1,1 +1,17 @@
 package ch01
+
+import "testing"
+
+func TestEx16(t *testing.T) {
+	for _, c := range []struct {
+		in   string
+		want int
+	}{
+		{in: "1984", want: 22},
+	} {
+		got := SumNumbers(c.in)
+		if got != c.want {
+			t.Errorf("SumNumbers(%v) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
