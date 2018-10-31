e49e9d42c749862392b09aae0901c808866a9200
jotranen <jari.otranen@gmail.com>
2018-10-31 13:40:15 +0200

    ch01 ex15

--- ch01/ex15.go
+++ ch01/ex15.go
@@ -1,6 +1,61 @@
 package ch01
 
+import (
+	"sort"
+)
+
 // Given a slice with 'n' elemenents & a value 'x', find two elements in the list that sums to 'x'
 // Hint:
 //	Approach 1: sort the list
 //	Approach 2: Using a hash table
+func Find2Sum(in []int, x int) []int {
+
+	var res []int
+	cpy := make([]int, len(in))
+	copy(cpy, in)
+
+	sort.Slice(cpy, func(i, j int) bool {
+		return in[i] < in[j]
+	})
+
+	var lsIndex int
+	// find largest smaller than x value
+	for i := len(cpy) - 1; i >= 0; i-- {
+		if cpy[i] < x {
+			lsIndex = i
+			break
+		}
+	}
+
+	// not found
+	if lsIndex == 0 {
+		return res
+	}
+
+	// find first part that sums up to x
+	iter := func(arr []int, start int, end int) int {
+		for i := start; i < end; i++ {
+			if arr[i] + arr[lsIndex] == x {
+				return i
+			}
+		}
+		return -1
+	}
+
+	sIndex := -1
+	for lsIndex > 0 {
+		sIndex = iter(cpy, 0, lsIndex)
+		if sIndex != -1 {
+			break
+		}
+		lsIndex--
+	}
+
+	if sIndex == -1 {
+		return res
+	}
+
+	res = []int{cpy[sIndex], cpy[lsIndex]}
+
+	return res
+}
--- ch01/ex15_test.go
+++ ch01/ex15_test.go
@@ -1,1 +1,41 @@
 package ch01
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestEx15(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		x 	int
+		want []int
+	}{
+		{in: []int{0, 1, 2}, x: 3, want: []int{1,2}},
+		{in: []int{2, 1, 0}, x: 3, want: []int{1,2}},
+		{in: []int{2, 0, 1}, x: 3, want: []int{1,2}},
+		{in: []int{0, 1, 3, 3, 4, 5, 6, 7, 8, 2}, x: 3, want: []int{1,2}},
+	} {
+
+		intComparer := func(a []int, b []int) bool {
+			if len(a) != len(b) {
+				return false
+			}
+			for i, v := range a {
+				if v != b[i] {
+					return false
+				}
+			}
+			return true
+		}
+
+		got := Find2Sum(c.in, c.x)
+
+		// make it a bit more testable
+		sort.Ints(got)
+
+		if !intComparer(got, c.want) {
+			t.Errorf("Find2Sum(%v, %d) == %v want %v", c.in, c.x, got, c.want)
+		}
+	}
+}
