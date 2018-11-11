2920bb7410596b6e41ad6e48894da3acc09d6f60
jotranen <jari.otranen@gmail.com>
2018-11-11 18:36:22 +0200

    ch06 bubblesort benchmark tests

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Exercises for book: DATA STRUCTURES & ALGORITHMS IN GO
 
-## Run
+## Run tests
 ``` 
 go test -v ./ch01
+```
+
+## Run benchmarks
+``` 
+go test -run=XXX -bench=BenchmarkBubbleSort2 ./ch06
 ```
 
 
--- ch06/bubblesort_test.go
+++ ch06/bubblesort_test.go
@@ -1,1 +1,29 @@
 package ch06
+
+import "testing"
+
+func BenchmarkBubbleSort1(b *testing.B) {
+
+	in := make([]int, 1000)
+	for i := 0; i < 1000; i++ {
+		in = append(in, i)
+	}
+	benchmarkBubbleSort(b, in)
+}
+
+func BenchmarkBubbleSort2(b *testing.B) {
+
+	in := make([]int, 1000)
+	for i := 1000; i > 0; i-- {
+		in = append(in, i)
+	}
+	benchmarkBubbleSort(b, in)
+}
+
+func benchmarkBubbleSort(b *testing.B, in []int) {
+
+	for n := 0; n < b.N; n++ {
+
+		BubbleSort(in, less)
+	}
+}
