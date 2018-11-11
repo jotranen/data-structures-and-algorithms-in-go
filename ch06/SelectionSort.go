036bd3773570257a6c890c457645b86143672d4c
jotranen <jari.otranen@gmail.com>
2018-11-12 00:18:00 +0200

    selection and insertion sort code

---
+++ ch06/SelectionSort.go
@@ -0,0 +1,15 @@
+package ch06
+
+func SelectionSort(arr []int) {
+	size := len(arr)
+	var i, j, max int
+	for i = 0; i < size - 1; i++ {
+		max = 0
+		for j = 1; j < size - 1 - i; j++ {
+			if arr[j] > arr[max] {
+				max = j
+			}
+		}
+		arr[size-1-i], arr[max] = arr[max], arr[size-1-i]
+	}
+}
---
+++ ch06/insertionsort.go
@@ -0,0 +1,13 @@
+package ch06
+
+func InsertionSort(arr []int, comp func(int, int) bool) {
+	size := len(arr)
+	var temp, i, j int
+	for i = 1; i < size; i ++ {
+		temp = arr[i]
+		for j = i; j > 0 && comp(arr[j-1], temp); j-- {
+			arr[j] = arr[j-1]
+		}
+		arr[j] = temp
+	}
+}
