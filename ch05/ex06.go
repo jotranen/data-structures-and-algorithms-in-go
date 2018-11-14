465225aa638f3ad06aeb1943be868ee4689333cc
jotranen <jari.otranen@gmail.com>
2018-11-14 23:31:36 +0200

    ch05 ex06

--- ch05/ex06.go
+++ ch05/ex06.go
@@ -1,5 +1,18 @@
 package ch05
 
+import "errors"
+
 // Give a list of elements in monotonically increasing with both negative and positive numbers.
 // Write an algorithm to find the point at which list becomes positive.
 
+var errFindMonotonicalList0Point = errors.New("not found")
+
+func FindMonotonicalList0Point(in []int) (int, error) {
+	for i := range in {
+		if in[i] > 0 {
+			return i, nil
+		}
+	}
+
+	return 0, errFindMonotonicalList0Point
+}
--- ch05/ex06_test.go
+++ ch05/ex06_test.go
@@ -1,1 +1,19 @@
-package ch05 
+package ch05
+
+import "testing"
+
+func TestFindMonotonicalList0Point(t *testing.T) {
+	for _, c := range []struct {
+		in   []int
+		want int
+		err  error
+	}{
+		{in: []int{-10, -9, 0, 1, 2}, want: 3},
+	} {
+		got, err := FindMonotonicalList0Point(c.in)
+
+		if got != c.want || err != c.err {
+			t.Errorf("TestFindMonotonicalList0Point(%v) = (%d, %v) want: (%d,%v) ", c.in, got, err, c.want, c.err)
+		}
+	}
+}
