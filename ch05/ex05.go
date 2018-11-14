30c7912824030cafae6e99878bd8f0e939f32cd3
jotranen <jari.otranen@gmail.com>
2018-11-14 20:30:19 +0200

    ch05 ex05

--- ch05/ex05.go
+++ ch05/ex05.go
@@ -1,4 +1,16 @@
 package ch05
 
 // Give a list of 0's, 1's and 2's, write a program to separate 0's, 1's and 2's.
+func Separate_0_1_2(in []int) (zeros, ones, twos int) {
 
+	for _, val := range in {
+		if val == 0 {
+			zeros++
+		} else if val == 1 {
+			ones++
+		} else {
+			twos++
+		}
+	}
+	return zeros, ones, twos
+}
--- ch05/ex05_test.go
+++ ch05/ex05_test.go
@@ -1,1 +1,20 @@
-package ch05 
+package ch05
+
+import "testing"
+
+func TestSeparate_0_1_2(t *testing.T) {
+	for _, c := range []struct {
+		in    []int
+		zeros int
+		ones  int
+		twos  int
+	}{
+		{in: []int{0, 1, 0, 1, 2, 0, 1}, zeros: 3, ones: 3, twos: 1},
+	} {
+		zeros, ones, twos := Separate_0_1_2(c.in)
+
+		if zeros != c.zeros || ones != c.ones {
+			t.Errorf("Separate_0_1(%v) = (%d, %d, %d), want: (%d, %d, %d)", c.in, zeros, ones, twos, c.zeros, c.ones, c.twos)
+		}
+	}
+}
