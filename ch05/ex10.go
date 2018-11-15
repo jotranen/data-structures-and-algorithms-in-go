428ef196da35d2dec98ddab32d392eb8aed0f630
jotranen <jari.otranen@gmail.com>
2018-11-15 22:41:08 +0200

    ch05 ex10

--- ch05/ex10.go
+++ ch05/ex10.go
@@ -1,4 +1,22 @@
 package ch05
 
-// Find kth smallest element in the union of two sorted list.
+import "sort"
 
+// Find kth smallest element in the union of two sorted list.
+func KthSmaller(in1 []int, in2 []int, k int) int {
+	cpy := make([]int, len(in1)+len(in2))
+
+	var i int
+	for _, v := range in1 {
+		cpy[i] = v
+		i++
+	}
+	for _, v := range in2 {
+		cpy[i] = v
+		i++
+	}
+
+	sort.Ints(cpy)
+
+	return cpy[k]
+}
