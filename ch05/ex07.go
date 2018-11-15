77196a2579ca63a6e8ce9655ab5057e946ba2211
jotranen <jari.otranen@gmail.com>
2018-11-15 22:33:28 +0200

    ch05 ex07

--- ch05/ex07.go
+++ ch05/ex07.go
@@ -2,3 +2,30 @@ package ch05
 
 // Give a sorted list, find a given number. If found return the index if not, find the index of that number if it is
 // inserted into the list.
+func FindNumberOrSlot(in []int, want int) (int, bool) {
+
+	i, j := 0, len(in)-1
+
+	var mid int
+
+	for i < j {
+		mid = (i + j) / 2
+		if in[mid] == want {
+			break
+		} else if in[mid] < want {
+			i = mid + 1
+		} else {
+			j = mid
+		}
+	}
+
+	if in[mid] == want {
+		return mid, true
+	} else {
+		if i > 0 && in[mid] < want {
+			return mid + 1, false
+		} else {
+			return mid, false
+		}
+	}
+}
--- ch05/ex07_test.go
+++ ch05/ex07_test.go
@@ -1,1 +1,22 @@
-package ch05 
+package ch05
+
+import "testing"
+
+func TestFindNumberOrSlot(t *testing.T) {
+	for _, c := range []struct {
+		in     []int
+		number int
+		want   int
+		ok     bool
+	}{
+		{in: []int{1, 2, 3, 5}, number: 4, want: 3, ok: false},
+		{in: []int{1, 2, 3, 5, 6}, number: 4, want: 3, ok: false},
+		{in: []int{1, 2, 3, 5, 6, 7}, number: 4, want: 3, ok: false},
+	} {
+		got, ok := FindNumberOrSlot(c.in, c.number)
+
+		if got != c.want || ok != c.ok {
+			t.Errorf("FindNumberOrSlot(%v, %d) = (%d, %t), want: (%d, %t)", c.in, c.number, got, ok, c.want, c.ok)
+		}
+	}
+}
