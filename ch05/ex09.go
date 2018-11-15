bb70b8ce9e8c7ece8db14c5b957fcf7b29840de2
jotranen <jari.otranen@gmail.com>
2018-11-15 22:37:39 +0200

    ch05 ex09

--- ch05/ex09.go
+++ ch05/ex09.go
@@ -1,4 +1,15 @@
 package ch05
 
+import "sort"
+
 // Find min in the sorted rotated list.
 
+func FindMin(in []int) int {
+	cpy := make([]int, len(in))
+	copy(cpy, in)
+
+	sort.Ints(cpy)
+
+	return cpy[0]
+
+}
