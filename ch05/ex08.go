2a61cb848e1a7d393809484f9e60c49800e9e1b9
jotranen <jari.otranen@gmail.com>
2018-11-15 22:37:01 +0200

    ch05 ex08

--- ch05/ex08.go
+++ ch05/ex08.go
@@ -1,3 +1,15 @@
 package ch05
 
+import "sort"
+
 // Find max in sorted rotated list.
+
+func FindMax(in []int) int {
+	cpy := make([]int, len(in))
+	copy(cpy, in)
+
+	sort.Ints(cpy)
+
+	return cpy[len(cpy)-1]
+
+}
